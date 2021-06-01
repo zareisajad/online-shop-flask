08b3011bc99b5ac61b7af72d5b3274af3ca9b7a8
sajad <zareisajad98@gmail.com>
2021-06-01 17:23:10 +0430

    add a new column called number, for counting number of a product added to cart

--- app/models.py
+++ app/models.py
@@ -12,3 +12,13 @@ class Products(db.Model):
     sold = db.Column(db.Integer)
     data_gallery = db.Column(db.LargeBinary)
     rendered_gallery = db.Column(db.Text)
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.LargeBinary)
+    rendered_data = db.Column(db.Text)
+    title = db.Column(db.String)
+    price = db.Column(db.Integer)
+    discunted = db.Column(db.Integer)
+    number = db.Column(db.Integer)
