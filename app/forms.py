1e4f7f93fa1601741e17b51cef8191a7037313e6
sajad <zareisajad98@gmail.com>
2021-06-01 17:22:06 +0430

    no required discounted price

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,10 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class AddProductForm(FlaskForm):
     photo = FileField('Photo:',validators=[FileRequired(),
-        FileAllowed(['jpg', 'png'], 'Images only!')])
+        FileAllowed(['jpg', 'png', 'gif', 'jpeg'], 'Images only!')])
     title = StringField('Title:', validators=[DataRequired(),length(min=1, max=39, message='Too many characters')])
     price = StringField('Price:', validators=[DataRequired()])
-    discunted = StringField('Discunted Price:', validators=[DataRequired()])
+    discunted = StringField('Discunted Price:')
     inventory = StringField('Inventory:', validators=[DataRequired()])
     photos = MultipleFileField('Add Gallery')
     submit = SubmitField('Publish')
