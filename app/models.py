261d3084b7d9aca97d2bc2f75a1355658c9be460
sajad <zareisajad98@gmail.com>
2021-06-12 13:58:37 +0430

    add category table and relation to products table

--- app/models.py
+++ app/models.py
@@ -10,6 +10,16 @@ class Products(db.Model):
     inventory = db.Column(db.Integer)
     sold = db.Column(db.Integer)
     gallery = db.relationship('Gallery', backref='gallery', lazy='dynamic')
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref='category')
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    
+    def __repr__(self):
+        return '{}'.format(self.name)
 
 
 class Gallery(db.Model):
