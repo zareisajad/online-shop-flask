24b4b62ca8424efb7b3fc56e8aefe4d236fa5e4a
sajad <zareisajad98@gmail.com>
2021-06-12 13:57:31 +0430

    add category form and select queryselectfield to the add product form

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, MultipleFileField
 from wtforms.validators import DataRequired, length
 from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from app.models import Category
+
+
+def enabled_categories():
+    return Category.query.all()
 
 
 class AddProductForm(FlaskForm):
@@ -11,7 +17,14 @@ class AddProductForm(FlaskForm):
     price = StringField('Price:', validators=[DataRequired()])
     discounted = StringField('Discunted Price:')
     inventory = StringField('Inventory:', validators=[DataRequired()])
+    category = QuerySelectField('Category',
+        query_factory=enabled_categories, allow_blank=True)
     photos = MultipleFileField(
         'Add Gallery',validators=[FileAllowed(
         ['jpg', 'png', 'gif', 'jpeg'], 'Images only!')])
     submit = SubmitField('Publish')
+
+
+class AddCategoryForm(FlaskForm):
+    name = StringField('Category name', validators=[DataRequired()])
+    submit = SubmitField('Add Category')
