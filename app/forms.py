fee2c6e8eb47e0b5449f9e2f00dd3f120db1e01b
sajad <zareisajad98@gmail.com>
2021-06-13 21:51:08 +0430

    add filter products form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, MultipleFileField
+from wtforms import StringField, SubmitField, MultipleFileField, RadioField
 from wtforms.validators import DataRequired, length
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
@@ -28,3 +28,16 @@ class AddProductForm(FlaskForm):
 class AddCategoryForm(FlaskForm):
     name = StringField('Category name', validators=[DataRequired()])
     submit = SubmitField('Add Category')
+
+
+class FilterProductsForm(FlaskForm):
+    keyword = StringField('Keyword:')
+    category = QuerySelectField('Category:',
+        query_factory=enabled_categories, allow_blank=True)
+    price_radio = RadioField('Price:', choices=[
+        ('value1','Most Sold'),
+        ('value2','Most Expensive'),('value3','Cheapest')])
+    property_radio = RadioField('Property:', choices=[
+        ('value1','Most Popular'),('value2','Newst')])
+    min_price = StringField('from:')
+    max_price = StringField('to')
