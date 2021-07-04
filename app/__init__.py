9ec862197ffcb6088cee9b1436763f9aa094957c
sajad <zareisajad98@gmail.com>
2021-07-04 15:26:33 +0430

    add flask login instance

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+login.login_message = 'برای مشاهد این صفحه ابتدا وارد حساب کاربری شوید'
+
 
 from app import app, routes
