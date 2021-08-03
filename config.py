4507de95bcf6e43cf840c09d71f60f6c6085280c
Sajad <zareisajad@protonmail.com>
2021-08-03 11:54:31 +0430

    add module and function docstring

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
+"""
+Class-based application configuration
+"""
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
+    """
+    Flask application config
+    """
+    # Flask settings
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # Flask-SQLAlchemy settings
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+        'sqlite:///' + os.path.join(basedir, 'app.db')  #File-based SQL database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Avoids SQLAlchemy warning
+
+    # Upload paths
     UPLOAD_PATH = os.path.join('app/static/images')
     UPLOAD_GALLERY = os.path.join('app/static/gallery')
     
