8c90421b0559647402512e6ef210c58193f48884
Sajad <zareisajad@protonmail.com>
2021-08-03 11:46:21 +0430

    add module docstring

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
+"""
+Flask application factory
+"""
+
 from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
