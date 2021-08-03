3a0c6c386573d125098f86fe05991f02ad8e8827
Sajad <zareisajad@protonmail.com>
2021-08-03 09:38:13 +0430

    add docstrings to methods

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import os
 import ast
 import datetime
+
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from sqlalchemy import desc, asc
@@ -14,8 +15,16 @@ from app.forms import AddProductForm, AddCategoryForm, FilterProductsForm,\
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    Register User
+    -------------
+    first if user is already registered, we redirect them to main page
+    after extract name and email given from the user, and saving in database
+    the hash of the password entered by user will store in db too.
+    set_password defined in forms.py
+    """
     if current_user.is_authenticated:
-        return redirect(url_for('products'))
+        return redirect(url_for('main_page'))
     form = RegisterationForm()
     if form.validate_on_submit():
         user = User(name=form.name.data,
@@ -30,8 +39,17 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Login User
+    -------------
+    first if user is already loged in, we redirect them to main page
+    after extract email and password from login form;
+    we check if the email is in db or not, otherwise app flashs a message
+    also we compare the given password with decoded hash that is in database.
+    check_password defined in forms.py
+    """
     if current_user.is_authenticated:
-        return redirect(url_for('products'))
+        return redirect(url_for('main_page'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -44,28 +62,43 @@ def login():
         login_user(user)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('products')
+            next_page = url_for('main_page')
         return redirect(next_page)
     return render_template('login.html', form=form)
 
 
 @app.route('/logout')
 def logout():
+    """
+    Logging Out
+    ---------------
+    we call logout_user() from flask login
+    redirect user to the main page, and that's it.
+    """
     logout_user()
-    return redirect(url_for('products'))
+    return redirect(url_for('main_page'))
 
 
 @app.route('/')
-def products():
-    pic = Products.query.all()
-    category = Category.query.all()
-    if not pic:
+def main_page():
+    """
+    Main Page
+    ---------
+    this page contains all the main_page cards.
+    if there is no products then we flash a message.
+    """
+    all_products = Products.query.all()
+    if not all_products:
         flash('محصولی موجود نیست')
-    return render_template('products.html', pic=pic, category=category)
+    return render_template('main_page.html', all_products=all_products)
 
 
 @app.route('/add-product', methods=['POST','GET'])
 def add_product():
+    """
+    Add New Product
+    ---------------
+    """
     form = AddProductForm()
     if form.validate_on_submit():
         c = Category.query.filter_by(name=str(form.category.data)).first()
@@ -88,12 +121,16 @@ def add_product():
             files = Gallery(pics=url, p_id=p.id)
             db.session.add(files)
         db.session.commit()
-        return redirect(url_for('products'))
+        return redirect(url_for('main_page'))
     return render_template('add_product.html', form=form)
 
 
 @app.route('/add-category', methods=['POST','GET'])
 def add_category():
+    """
+    Add New Category
+    ---------------
+    """
     form = AddCategoryForm()
     if form.validate_on_submit():
         category = Category(name=form.name.data)
@@ -187,6 +224,10 @@ def newst_filter():
 
 @app.route('/manage', methods=['POST','GET'])
 def manage_products():
+    """
+    Manage Products Page
+    ---------------
+    """
     all_products = Products.query.all()
     if not products:
         flash('محصولی موجود نیست')
@@ -195,6 +236,10 @@ def manage_products():
 
 @app.route('/del/<int:id>', methods=['GET', 'POST'])
 def delete(id):
+    """
+    Delete Products
+    ---------------
+    """
     g = Gallery.query.all()
     for i in g:
         if i.p_id == id:
@@ -215,6 +260,10 @@ def delete(id):
 
 @app.route('/<int:id>',methods=['GET', 'POST'])
 def product_detail(id):
+    """
+    Products Details
+    ---------------
+    """
     product = Products.query.filter_by(id=id).first()
     gallery = Gallery.query.all()
     category = Category.query.filter_by(id=product.category_id).first()
@@ -228,7 +277,8 @@ def orders_list():
     """
     show all orders
     ---------------
-    query the Orders table and find all the users that have any order.
+    query the Orders table and find
+    all the users that have any order.
     """
     orders = Orders.query.filter(Orders.orders_id==User.id).all()
     users = [User.query.filter(User.id==i.orders_id).first() for i in orders]
@@ -259,6 +309,10 @@ def order_line(id):
 @app.route('/user-checkout', methods=['POST','GET'])
 @login_required
 def checkout():
+    """
+    Checkout Page
+    ---------------
+    """
     form = CheckoutForm()
     c = Cart.query.filter(Cart.cart_id==current_user.id).all()
     p =[Products.query.filter(Products.id==i.product_id).first() for i in c]
@@ -268,6 +322,10 @@ def checkout():
 @app.route('/user-payment', methods=['POST','GET'])
 @login_required
 def payment():
+    """
+    Payment Page
+    ---------------
+    """
     form = CheckoutForm()
     o = Orders.query.filter(Orders.orders_id==current_user.id).first()
     c = Cart.query.filter(Cart.cart_id==current_user.id).all()
@@ -296,36 +354,44 @@ def payment():
             Orders.orders_id==current_user.id).first()
         orders.payment_method = 'نقدی'
         db.session.commit()
-        return redirect(url_for('products'))
+        return redirect(url_for('main_page'))
     if o:
         flash('این سفارش قبلا ثبت شده است')
-        return redirect(url_for('products'))
+        return redirect(url_for('main_page'))
     return redirect(url_for('payment_gateway', name='None'))
 
 
 @app.route('/payment-gateway/<name>', methods=['POST','GET'])
 @login_required
 def payment_gateway(name):
+    """
+    Fake Payment Gateway. *Temporary*
+    ---------------
+    """
     o = Orders.query.filter(Orders.orders_id==current_user.id).first()
     if name == 'پرداخت':
         order = Orders.query.filter(Orders.orders_id==current_user.id).first()
         order.status = 'پرداخت شده'
         flash('پرداخت موفق')
         db.session.commit()
-        return redirect(url_for('products'))
+        return redirect(url_for('main_page'))
     if name == 'انصراف':
         order = Orders.query.filter(
             Orders.orders_id==current_user.id).first()
         order.status = 'کنسل شده'
         flash('عملیات پرداخت کنسل شد')
         db.session.commit()
-        return redirect(url_for('products'))
+        return redirect(url_for('main_page'))
     return render_template('gateway.html', o=o)
 
 
 @app.route('/cart/<int:id>', methods=['POST','GET'])
 @login_required
 def cart(id):
+    """
+    Add Products To Cart
+    ---------------
+    """
     ca = Cart.query.filter(
         Cart.product_id==id, Cart.cart_id==current_user.id).first()
     if ca:
@@ -341,6 +407,10 @@ def cart(id):
 
 @app.route('/user-<int:id>', methods=['POST','GET'])
 def show_cart(id):
+    """
+    User Cart
+    ---------------
+    """
     # get all the items that matchs with current_user.id in Cart table
     c = Cart.query.filter(Cart.cart_id==current_user.id).all()
     if not c:
@@ -352,6 +422,10 @@ def show_cart(id):
 
 @app.route('/del/cart/<int:id>', methods=['GET', 'POST'])
 def delete_cart(id):
+    """
+    Remove Items From Cart
+    ---------------
+    """
     c = Cart.query.filter(Cart.cart_id==current_user.id,
                           Cart.product_id==id).first()
     db.session.delete(c)
@@ -361,6 +435,10 @@ def delete_cart(id):
 
 @app.route('/fa', methods=['GET', 'POST'])
 def final_amount():
+    """
+    Calculate Final Amount
+    ---------------
+    """
     c = Cart.query.filter(Cart.cart_id==current_user.id).all()
     p = [Products.query.filter(Products.id==i.product_id).first() for i in c]
     total = []
@@ -379,6 +457,10 @@ def final_amount():
 
 @app.route('/c/add/<int:id>', methods=['POST','GET'])
 def add_num(id):
+    """
+    Add Product Number In Cart
+    ---------------
+    """
     c = Cart.query.filter(Cart.product_id==id,
                           Cart.cart_id==current_user.id).first()
     p = Products.query.filter(Products.id==id).first()
@@ -394,6 +476,10 @@ def add_num(id):
 
 @app.route('/c/reduce/<int:id>', methods=['POST','GET'])
 def reduce_num(id):
+    """
+    Reduce Product Number In Cart
+    ---------------
+    """
     c = Cart.query.filter(Cart.product_id==id,
                           Cart.cart_id==current_user.id).first()
     p = Products.query.filter(Products.id==id).first()
@@ -409,15 +495,23 @@ def reduce_num(id):
 
 @app.route('/like/<int:id>', methods=['POST','GET'])
 def add_rate(id):
+    """
+    Add Products Rate
+    ---------------
+    """
     p = Products.query.filter_by(id=id).first()
     p.rate += 1
     db.session.commit()
-    return redirect(url_for('products'))
+    return redirect(url_for('main_page'))
 
 
 @app.route('/dislike/<int:id>', methods=['POST','GET'])
 def reduce_rate(id):
+    """
+    Reduce Product Rate
+    ---------------
+    """
     p = Products.query.filter_by(id=id).first()
     p.rate -= 1
     db.session.commit()
-    return redirect(url_for('products'))
+    return redirect(url_for('main_page'))
