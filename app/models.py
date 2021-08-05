7952a6b2c09f895c50656c88a9a232459fa32783
sajadzarei <zareisajad@protonmail.com>
2021-08-05 21:04:25 +0430

    add flask-user, requireds to the User table, also add Role and UserRoles models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from app import db, login, app
 from datetime import datetime
-from flask_login import UserMixin
 
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_user import UserMixin
+
+from app import db, login, app
 
 
 class Products(db.Model):
@@ -26,18 +27,39 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    active = db.Column(
+        'is_active', db.Boolean(), nullable=False, server_default='1')
+    email_confirmed_at = db.Column(db.DateTime())
     cart = db.relationship('Cart', backref='cart')
     orders = db.relationship('Orders', backref='orders')
+    roles = db.relationship('Role', secondary='user_roles')
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
+
+
+# Define the Role data-model
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(
+        db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(
+        db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
 
 class Cart(db.Model):
