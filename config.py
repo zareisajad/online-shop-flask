d5890bf2d009c9eec85b6effe9fde32d4a3e7c03
sajad <zareisajad@protonmail.com>
2021-08-11 16:35:25 +0430

    set limit for upload file

--- config.py
+++ config.py
@@ -9,16 +9,15 @@ class Config(object):
     """
     # Flask settings
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
     # Flask-SQLAlchemy settings
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db') #File-based SQL database
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Avoids SQLAlchemy warning
-
+    # set size limit for upload files
+    MAX_CONTENT_LENGTH = 5 * 1000 * 1000
     # Upload paths
     UPLOAD_PATH = os.path.join('app/static/images')
     UPLOAD_GALLERY = os.path.join('app/static/gallery')
-
     # Flask-User settings
     USER_APP_NAME = "بازدید سایت"
     USER_ENABLE_EMAIL = True # Enable email authentication
