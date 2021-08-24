35b84c857b1df33737a3ec69bd9eb2f1fcc4f0ee
sajad <zareisajad@protonmail.com>
2021-08-24 22:33:47 +0430

    updated templates and photo ulr links to show pics currectly

--- config.py
+++ config.py
@@ -12,11 +12,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     # Flask-SQLAlchemy settings
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db') #File-based SQL database
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # Avoids SQLAlchemy warning
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Avoids SQLAlchemy warning
     # set size limit for upload files
     MAX_CONTENT_LENGTH = 3 * 1000 * 1000
     # Upload paths
     UPLOAD_PATH = os.path.join('app/static/images')
     UPLOAD_GALLERY = os.path.join('app/static/gallery')
-
